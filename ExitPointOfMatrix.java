1396032b58a7ad703dd3f1184d17efce3cb0f79d
anjalirai530 <raianjali1201@gmail.com>
2020-10-05 21:24:51 +0530

    Create ExitPointOfMatrix.java

---
+++ ExitPointOfMatrix.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int arr[][] = new int[m][n];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0 ; j < arr[0].length;j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        int dir = 0;
+        int i = 0;
+        int j = 0;
+        while(true){
+            dir = (arr[i][j] + dir)%4;
+            
+            if(dir == 0){
+                j++;
+            }else if(dir == 1){
+                i++;
+            }else if(dir == 2){
+                j--;
+            }else if(dir == 3){
+                i--;
+            }
+            
+            if(i<0){
+                i++;
+                break;
+            }else if(j<0){
+                j++;
+                break;
+            }else if(i == arr.length){
+                i--;
+                break;
+            }else if(j == arr[0].length){
+                j--;
+                break;
+            }
+        }
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+}

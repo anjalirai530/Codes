56f723939f92a4dde3338911827e469c78d82040
anjalirai530 <raianjali1201@gmail.com>
2020-10-13 18:52:41 +0530

    Create queue-to-stack-adapter-pop-efficient.java

---
+++ queue-to-stack-adapter-pop-efficient.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class QueueToStackAdapter {
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+
+    public QueueToStackAdapter() {
+      mainQ = new ArrayDeque<>();
+      helperQ = new ArrayDeque<>();
+    }
+
+    int size() {
+        return mainQ.size();
+      // write your code here
+    }
+
+    void push(int val) {
+       
+            while(mainQ.size() >0){
+                helperQ.add(mainQ.remove());
+            }
+            mainQ.add(val);
+            while(helperQ.size() > 0){
+                mainQ.add(helperQ.remove());
+            }
+        
+      // write your code here
+    }
+
+    int pop() {
+        if(mainQ.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        return mainQ.remove();
+       
+      // write your code here
+    }
+
+    int top() {
+         if(mainQ.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        return mainQ.peek();
+      // write your code here
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    QueueToStackAdapter st = new QueueToStackAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("push")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if (str.startsWith("pop")) {
+        int val = st.pop();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("top")) {
+        int val = st.top();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(st.size());
+      }
+      str = br.readLine();
+    }
+  }
+}

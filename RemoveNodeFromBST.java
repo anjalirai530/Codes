cf86c7f480aaa6f0bb90042b166538519f435b61
anjalirai530 <raianjali1201@gmail.com>
2020-11-02 11:32:19 +0530

    Create RemoveNodeFromBST.java

---
+++ RemoveNodeFromBST.java
@@ -0,0 +1,31 @@
+ public static int max(Node node){
+      if(node.right != null){
+          return max(node.right);
+      }else{
+          return node.data;
+      }
+  }
+  public static Node remove(Node node, int data) {
+      if(node == null){
+          return null;
+      }
+      
+      if(node.data > data){
+          node.left = remove(node.left,data);
+      }else if(node.data < data){
+          node.right = remove(node.right,data);
+      }else{
+          if(node.left != null && node.right != null){
+              int lmax = max(node.left);
+              node.data = lmax;
+              node.left = remove(node.left,lmax);
+          }else if(node.left != null){
+              return node.left;
+          }else if(node.right !=null){
+              return node.right;
+          }else{
+              return null;
+          }
+      }
+      return node;
+  }

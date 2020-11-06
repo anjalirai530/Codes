03d1debfc54fe08835d9a6bd270b0bbc353c0cc9
anjalirai530 <raianjali1201@gmail.com>
2020-11-06 10:15:10 +0530

    Create CoinChangePermutations.java

---
+++ Dp/CoinChangePermutations.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+     Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+     int arr[] = new int[n];
+     for(int i = 0; i < arr.length; i++){
+         arr[i] = scn.nextInt();
+     }
+     int val = scn.nextInt();
+     int dp[] = new int[val + 1];
+     dp[0] = 1;
+     for(int j = 1; j < dp.length; j++){
+         for(int i = 0;i<arr.length;i++){
+             if(arr[i] <= j){
+                 dp[j] = dp[j] + dp[j - arr[i]];
+             }
+         }
+     }
+     System.out.println(dp[val]);
+    }
+}
